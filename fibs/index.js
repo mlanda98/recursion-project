487bf237664864637f659f10075f7b267c6839ef
Mayra Landa <mlandae16@gmail.com>
2024-05-23 11:10:57 -0700

    add fibs recursion function

--- fibs/index.js
+++ fibs/index.js
@@ -12,3 +12,18 @@ function fibs(num) {
 };
 
 console.log(fibs(4));
+
+function fibRec(num, fibArray = [0, 1]){
+  if (num <= 0){
+    return [];
+  } else if (num === 1){
+    return [0];
+  } else if (fibArray.length >= num){
+    return fibArray.slice(0, num);
+  }
+
+  const nextFib = fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
+  fibArray.push(nextFib);
+  return fibRec(num, fibArray);
+}
+console.log(fibRec(4));
